4500f3d3992a7ae95371ddd7e2327ff6705f4c38
Elben Shira <elbenshira@gmail.com>
2019-03-31 10:09:51 -0500

    Specify hsass 0.8.0 for ghc 8.4.
    The nixos-18.09 channel has hsass 0.7.0. But pencil requires
    hsass >= 0.8.

--- release-ghc-8.4.nix
+++ release-ghc-8.4.nix
@@ -3,4 +3,19 @@ let
 in
   buildPkg {
     channel = "nixos-18.09";
+
+    # Following https://github.com/Gabriel439/haskell-nix/tree/master/project1
+    config = {
+      packageOverrides = pkgs: rec {
+        haskellPackages = pkgs.haskellPackages.override {
+          overrides = haskellPackagesNew: haskellPackagesOld: rec {
+            # Since pencil 0.1.3 specifies hsass >= 0.8, and the 18.09 nix
+            # channel only has hsass 0.7.
+            # cabal2nix cabal://hsass-0.8.0 > hsass-0.8.0.nix
+            hsass =
+              haskellPackagesNew.callPackage ./nix/ghc-8.4/hsass-0.8.0.nix { };
+          };
+        };
+      };
+    };
   }
